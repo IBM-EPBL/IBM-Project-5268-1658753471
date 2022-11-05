2c822247dfc363fecacd352b0c11698427b13130
ganesh505 <85790859+ganesh505@users.noreply.github.com>
2022-11-05 20:47:41 +0530

    Create code.py

---
+++ Project Development Phase/Sprint 2/Code./code.py
@@ -0,0 +1,58 @@
+import time
+import sys
+import ibmiotf.application
+import ibmiotf.device
+
+
+
+
+organization = "br1jua"
+deviceType = "harish123"
+deviceId = "123"
+authMethod = "token"
+authToken = "harish123"
+
+
+
+temp=60
+pulse=70
+oxygen= 30
+lat =  17
+lon = 18
+
+
+def myCommandCallback(cmd):
+    print("Command received: %s" % cmd.data['command'])
+    print(cmd)
+        
+
+
+try:
+	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+	deviceCli = ibmiotf.device.Client(deviceOptions)
+	#..............................................
+	
+except Exception as e:
+	print("Caught exception connecting device: %s" % str(e))
+	sys.exit()
+
+
+deviceCli.connect()
+
+while True:
+        #Get Sensor Data from DHT11
+        
+        data = {"d":{ 'temp' : temp, 'pulse': pulse ,'oxygen': oxygen,"lat":lat,"lon":lon}}
+        #print data
+        def myOnPublishCallback():
+            print ("Published Temperature = %s C" % temp, "Humidity = %s %%" % pulse, "to IBM Watson")
+
+        success = deviceCli.publishEvent("IoTSensor", "json", data, qos=0, on_publish=myOnPublishCallback)
+        if not success:
+            print("Not connected to IoTF")
+        time.sleep(1)
+        
+        deviceCli.commandCallback = myCommandCallback
+
+
+deviceCli.disconnect()
